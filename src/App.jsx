380605e1dcc51fcccbd4c744d1428d6a62cce949
majidhamuhammed <majidhamuhammed122@gmail.com>
2025-06-09 13:58:28 +0530

    link route fix in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
-import Footer from './Components/Footer';
+import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddStudent from './pages/AddStudent';
 import StudentList from './pages/StudentList';
